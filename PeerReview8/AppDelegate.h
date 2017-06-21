07516ebedc4538569b1aec3fe54a5621a28a5248
Ananta Shahane <anantashahane@Anantas-MacBook-Air.local>
2017-06-21 18:55:07 +0530

    Initial Commit

---
+++ PeerReview8/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PeerReview8
+//
+//  Created by Ananta Shahane on 6/21/17.
+//  Copyright © 2017 Ananta Shahane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
